72d0e9a07bea6ff3ce74868d598071f12e454f2e
Daniel Boley <danboley1@gmail.com>
2024-08-20 14:08:30 -0400

    establish node.js server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.listen(5005, () => {
+  console.log("Server started on port 5005");
+});
