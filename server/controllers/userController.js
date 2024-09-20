cfc14db04dab50967189cfba5d065cd1b62e8514
Daniel Boley <danboley1@gmail.com>
2024-09-20 10:43:31 -0400

    additional error handling for get routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,6 +28,9 @@ const createUser = async (req, res) => {
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll();
+    if (users.length === 0) {
+      return res.status(200).json({ message: "No users found" });
+    }
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
